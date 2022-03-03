32ed43f0e6ac23043071570912508e66a9e1e5a0
Kevin OLIKOYI <kevinolikoyi@gmailcom>
2022-03-03 09:01:06 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+import 'materialize-css/dist/css/materialize.min.css'
+import 'material-design-icons/iconfont/material-icons.css'
 
 export default function (Vue, { router, head, isClient }) {
+
+  
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 }
